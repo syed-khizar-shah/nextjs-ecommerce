43f985ffc346c5c7e2dbda177fa5a3d61200bf60
Syed-Khizar-Ali-Shah <115891972+Syed-Khizar-Ali-Shah@users.noreply.github.com>
2025-02-08 21:13:45 +0500

    first commit

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,5 +1,4 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 import { auth } from "@/auth";
 
 const f = createUploadthing();
@@ -12,16 +11,16 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    // .middleware(async () => {
-    //   // This code runs on your server before upload
-    //   const session = await auth();
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const session = await auth();
 
-    //   // If you throw, the user will not be able to upload
-    //   // if (!session) throw new UploadThingError("Unauthorized");
+      // If you throw, the user will not be able to upload
+      // if (!session) throw new UploadThingError("Unauthorized");
 
-    //   // Whatever is returned here is accessible in onUploadComplete as `metadata`
-    //   return { userId: session?.user?.id };
-    // })
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session?.user?.id };
+    })
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("📂 Incoming File:", file); // Print file details
